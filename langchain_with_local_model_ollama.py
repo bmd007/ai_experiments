3e2fdb676debf8eb2656c045f4a957f842923da2
Mahdi <bmd579@gmail.com>
2023-11-11 21:51:21 +0100

    ollama

---
+++ langchain_with_local_model_ollama.py
@@ -0,0 +1,61 @@
+from langchain import hub
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import JSONLoader
+from langchain.embeddings import OllamaEmbeddings
+from langchain.llms import Ollama
+from langchain.schema import LLMResult
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+
+# load log file
+loader = JSONLoader(file_path="/Users/mohami/workspacce/personal-repositories/ai_experiments/local_data/small.json",
+                    jq_schema=".[]",
+                    text_content=False)
+data = loader.load()
+# Split into chunks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=6000, chunk_overlap=300)
+all_splits = text_splitter.split_documents(data)
+# Embed and store
+vectorstore = Chroma.from_documents(documents=all_splits,
+                                    embedding=OllamaEmbeddings(base_url="http://localhost:11434", model="llama2")
+                                    )
+# result = vectorstore.similarity_search(query="challenge", k=1)
+# print(result)
+#####
+# RAG prompt
+QA_CHAIN_PROMPT = hub.pull("rlm/rag-prompt-llama")
+
+
+class GenerationStatisticsCallback(BaseCallbackHandler):
+    def on_llm_end(self, response: LLMResult, **kwargs) -> None:
+        print(response.generations[0][0].generation_info)
+
+
+callback_manager = CallbackManager(
+    [
+        StreamingStdOutCallbackHandler(),
+        # GenerationStatisticsCallback()
+    ]
+)
+
+llm = Ollama(
+    base_url="http://localhost:11434",
+    model="llama2",
+    verbose=True,
+    callback_manager=callback_manager,
+    temperature=0
+)
+
+qa_chain = RetrievalQA.from_chain_type(
+    llm,
+    retriever=vectorstore.as_retriever(),
+    chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
+)
+
+question = "Any customer having problem with enrollment? show the corresponding customerId and other identifiers if any"
+# question = "Any errors"
+# question = "what's going on with customer xxx"
+result = qa_chain({"query": question})
--- log-explorer/src/main/java/io/bmd007/ai/logexplorer/LogExplorerApplication.java
+++ log-explorer/src/main/java/io/bmd007/ai/logexplorer/LogExplorerApplication.java
@@ -1,5 +1,6 @@
 package io.bmd007.ai.logexplorer;
 
+import com.hexadevlabs.gpt4all.LLModel;
 import de.kherud.llama.InferenceParameters;
 import de.kherud.llama.LlamaModel;
 import de.kherud.llama.ModelParameters;
@@ -13,49 +14,91 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 
 @Slf4j
 @SpringBootApplication
 public class LogExplorerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LogExplorerApplication.class, args);
-	}
+    private static final String LUNA_AI_LLAMA2_Path = "/Users/mohami/workspacce/personal-repositories/ai_experiments/models/luna-ai-llama2";
+    private static final String DOLPHON_MISTRAL_Path = "/Users/mohami/workspacce/personal-repositories/ai_experiments/models/dolphin-2.2.1-mistral-7b.Q8_0.gguf";
+    private static final String MISTRAL_OPENORCA_PATH = "/Users/mohami/workspacce/personal-repositories/ai_experiments/models/mistral-7b-openorca.Q4_0.gguf";
 
-	@EventListener(ApplicationReadyEvent.class)
-	public void onApplicationReady() throws IOException {
+    public static void main(String[] args) {
+        SpringApplication.run(LogExplorerApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReady() {
+//        gpt4allBridge(MISTRAL_OPENORCA_PATH);
+        llamaCPPBridge(LUNA_AI_LLAMA2_Path);
+    }
+
+    void gpt4allBridge(String modelPath) {
+        String prompt = "### Human:\nWhat is the meaning of life\n### Assistant:";
+
+        try (LLModel model = new LLModel(Path.of(modelPath))) {
+
+            LLModel.GenerationConfig config = LLModel.config()
+                    .withNPredict(4096)
+                    .withTemp(0)
+                    .build();
+
+            String fullGeneration = model.generate(prompt, config, true);
+
+        } catch (Exception e) {
+            // Exceptions generally may happen if the model file fails to load
+            // for a number of reasons such as a file not found.
+            // It is possible that Java may not be able to dynamically load the native shared library or
+            // the llmodel shared library may not be able to dynamically load the backend
+            // implementation for the model file you provided.
+            //
+            // Once the LLModel class is successfully loaded into memory the text generation calls
+            // generally should not throw exceptions.
+            e.printStackTrace(); // Printing here but in a production system you may want to take some action.
+        }
+
+    }
+
+    void llamaCPPBridge(String modelPath) {
 //		LlamaModel.setLogger((level, message) -> System.out.print(message));
-		ModelParameters modelParams = new ModelParameters.Builder()
-				.setNGpuLayers(43)
-				.build();
-		InferenceParameters inferParams = new InferenceParameters.Builder()
-				.setTemperature(0.7f)
-				.setPenalizeNl(true)
-				.setMirostat(InferenceParameters.MiroStat.V2)
-				.setAntiPrompt("\n")
-				.build();
+        ModelParameters modelParams = new ModelParameters.Builder()
+                .setNGpuLayers(43)
+                .build();
+        InferenceParameters inferParams = new InferenceParameters.Builder()
+                .setTemperature(0f)
+                .setPenalizeNl(true)
+                .setMirostat(InferenceParameters.MiroStat.V2)
+                .setAntiPrompt("\n")
+                .build();
 
-		String modelPath = "/Users/mohami/workspacce/personal-repositories/ai_experiments/models/luna-ai-llama2";
-		String system = "This is a conversation between User and Llama, a friendly chatbot.\n" +
-				"Llama is helpful, kind, honest, good at writing, and never fails to answer any " +
-				"requests immediately and with precision.\n";
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
-		try (LlamaModel model = new LlamaModel(modelPath, modelParams)) {
-			System.out.print(system);
-			String prompt = system;
-			while (true) {
-				prompt += "\nUser: ";
-				System.out.print("\nUser: ");
-				String input = reader.readLine();
-				prompt += input;
-				System.out.print("Llama: ");
-				prompt += "\nLlama: ";
-				for (LlamaModel.Output output : model.generate(prompt, inferParams)) {
-					System.out.print(output.text);
-					prompt += output;
-				}
-			}
-		}
-	}
-
+        String system = """
+                This is a conversation between a human (user) and you (Llama), a friendly chatbot.
+                Llama is helpful, kind, honest, good at writing, and never fails to answer any requests immediately and with precision.
+                If you don't know the answer, respond by a request for search like following:
+                {
+                    "messageType": "searchRequest",
+                    "value": ${what you don't know}
+                }
+                """;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+        try (LlamaModel model = new LlamaModel(modelPath, modelParams)) {
+            System.out.print(system);
+            String prompt = system;
+            while (true) {
+                prompt += "\nUser: ";
+                System.out.print("\nUser: ");
+                String input = reader.readLine();
+                prompt += input;
+                System.out.print("Llama: ");
+                prompt += "\nLlama: ";
+                for (LlamaModel.Output output : model.generate(prompt, inferParams)) {
+                    System.out.print(output.text);
+                    prompt += output;
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
