4b3c07b206f1749494051f58dff33d97b9581d47
Mahdi <bmd579@gmail.com>
2024-04-08 23:23:22 +0200

    spring ai

---
+++ java/spring-ai-locally/src/main/java/io/github/bmd007/experiment/ai/springailocally/SpringAiLocallyApplication.java
@@ -0,0 +1,31 @@
+package io.github.bmd007.experiment.ai.springailocally;
+
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@SpringBootApplication
+public class SpringAiLocallyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiLocallyApplication.class, args);
+    }
+
+    @Autowired
+    private OllamaChatClient chatClient;
+
+    @GetMapping("/ai/generate")
+    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        return chatClient.stream(prompt);
+    }
+}
---
+++ java/spring-ai-locally/src/test/java/io/github/bmd007/experiment/ai/springailocally/SpringAiLocallyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bmd007.experiment.ai.springailocally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiLocallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
