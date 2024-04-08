77f75035fedbcbeb9d2d053975e19ed919ba9298
Mahdi <bmd579@gmail.com>
2024-04-08 23:50:55 +0200

    spring ai

--- java/spring-ai-locally/src/main/java/io/github/bmd007/experiment/ai/springailocally/SpringAiLocallyApplication.java
+++ java/spring-ai-locally/src/main/java/io/github/bmd007/experiment/ai/springailocally/SpringAiLocallyApplication.java
@@ -1,18 +1,21 @@
 package io.github.bmd007.experiment.ai.springailocally;
 
-import org.springframework.ai.chat.ChatResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.embedding.EmbeddingClient;
+import org.springframework.ai.embedding.EmbeddingResponse;
 import org.springframework.ai.ollama.OllamaChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import reactor.core.publisher.Flux;
 
-@RestController
+import java.util.List;
+
+@Slf4j
 @SpringBootApplication
 public class SpringAiLocallyApplication {
 
@@ -23,9 +26,23 @@ public class SpringAiLocallyApplication {
     @Autowired
     private OllamaChatClient chatClient;
 
-    @GetMapping("/ai/generate")
-    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+    @Autowired
+    private EmbeddingClient embeddingClient;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReady() {
+        String message = "Hello";
+        EmbeddingResponse embeddingResponse = embeddingClient.embedForResponse(List.of(message));
+        Flux.fromIterable(embeddingResponse.getResult().getOutput())
+                .subscribe(aDouble -> log.info("embedding {}", aDouble));
+
+        log.info("embedding Hello {}", embeddingClient.embed("Hello"));
+
         Prompt prompt = new Prompt(new UserMessage(message));
-        return chatClient.stream(prompt);
+        chatClient.stream(prompt)
+                .flatMap(chatResponse -> Flux.fromIterable(chatResponse.getResults()))
+                .map(chatResult -> chatResult.getOutput().getContent())
+                .doOnNext(chatResult -> log.info("chatResult {}", chatResult))
+                .subscribe();
     }
 }
